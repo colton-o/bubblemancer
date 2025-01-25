31f6334f007fcbe34d92460490da7e54f620a396
colton <colton.o@hotmail.com>
2025-01-25 11:39:07 +0000

    basic gameplay set

--- Objects/orb.gd
+++ Objects/orb.gd
@@ -1,13 +1,34 @@
-extends Node2D
-@export var speed = 200
+extends CharacterBody2D
+@export var speed = 0
+var active = true
+var collision
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	velocity = Vector2(0, -1*speed).rotated(rotation)
+	
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	position.y -= speed * delta
+	
+			
+func _physics_process(delta: float) -> void:
+	if active:
+		collision = move_and_collide(velocity*delta)
+	else: 
+		collision = null
+	if(collision):
+		var colName = collision.get_collider().name
+		print(colName)
+		if colName == "walls":
+			velocity = velocity.bounce(collision.get_normal())
+			print("wall")
+		else:
+			velocity = Vector2.ZERO
+			process_mode = 4
+			active = false
+	
+		
+	
+	
 	
 	
