4d2d464801e743d5cfb12ff5b836073bd51ca8ea
MilesJHansen <milesjhansen@gmail.com>
2025-01-25 12:16:28 +0800

    Update orb.gd
    add connected orb list

--- Objects/orb.gd
+++ Objects/orb.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
+class_name Orb
 @export var speed = 0
 var active = true
 var collision
+var connectedOrbs = Array([], TYPE_OBJECT, "Node", Orb)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,11 +24,17 @@ func _physics_process(delta: float) -> void:
 		if colName == "walls":
 			velocity = velocity.bounce(collision.get_normal())
 			print("wall")
+		elif colName == "orb":
+			connectedOrbs.append(collision)
+			collision.connectedOrbs.append(self)
+			velocity = Vector2.ZERO
+			process_mode = 4
+			active = false
+			print("orb")
 		else:
 			velocity = Vector2.ZERO
 			process_mode = 4
 			active = false
-	
 		
 	
 	
